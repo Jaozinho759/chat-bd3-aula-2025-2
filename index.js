c9c2381caea3d65cef410f57162431dd941f1dd3
Jaozinho759 <167478430+Jaozinho759@users.noreply.github.com>
2025-04-29 20:39:03 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,25 +1,44 @@
 const express = require('express')
-
 const http = require('http');
-
-const app = express();
-
+const socket = require('socket.io')
 const ejs = require('ejs')
 const path = require('path')
 
-app.use(express.static(path.join(__dirname, 'public')));
-
-
+const app = express();
 const server = http.createServer(app);
+const io = socket(server);
+
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.set('view', path.join(__dirname, 'public'))
 
 app.engine('html', ejs.renderFile);
 
-app.use('/', (req, res) =>{
+app.use('/', (req, res) => {
     res.render('index.html');
 })
 
-server.listen(3000, ()=>{
+/* LOGICA DE SOCKE.IO EMVIO E PROPAGAÇÃO DE MENSAGENS*/
+
+/* ESTRUTURA DE CONEXÃO DO SOCKET.IO*/
+
+let menssagens = []
+
+io.on('connection', socket => {
+    console.log("NOVO USUÁRIO CONECTADO: " + socket.id);
+
+    socket.emit('previousMessage', menssagens);
+
+    socket.on('sendMessage', data => {
+        menssagens.push(data)
+
+        socket.broadcast.emit('reciveMessage', data);
+    })
+
+
+});
+
+server.listen(3000, () => {
     console.log('CHAT RODANDO EM - http://localhost:3000')
 });
